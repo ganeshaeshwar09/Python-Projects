eab3a9a628ccb22f26ddcf981bc0d3fcecd731e0
ganeshaeshwar09 <ganeshaeshwar09@gmail.com>
2024-08-05 19:32:33 -0400

    Practise codes

--- Interactive_Expense_Calculator/app.py
+++ Interactive_Expense_Calculator/app.py
@@ -3,23 +3,49 @@ from expense_tracker import Expense, load_expenses, save_expenses
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     expenses = load_expenses()
     return render_template('index.html', expenses=expenses)
 
+
 @app.route('/submit', methods=['POST'])
 def submit():
+    date = request.form['date']
+    category = request.form['category']
+    description = request.form['description']
+    amount = float(request.form['amount'])
+    expenses = load_expenses()
+    expense = Expense(date, category, description, amount)
+    expenses.append(expense)
+    save_expenses(expenses)
+    return redirect(url_for('index'))
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    expenses = load_expenses()
+    expense = expenses[id]
+
     if request.method == 'POST':
-        date = request.form['date']
-        category = request.form['category']
-        description = request.form['description']
-        amount = float(request.form['amount'])
-        expenses = load_expenses()
-        expense = Expense(date, category, description, amount)
-        expenses.append(expense)
+        expenses[id].date = request.form['date']
+        expenses[id].category = request.form['category']
+        expenses[id].description = request.form['description']
+        expenses[id].amount = float(request.form['amount'])
         save_expenses(expenses)
         return redirect(url_for('index'))
+
+    return render_template('edit.html', id=id, expense=expense)
+
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    expenses = load_expenses()
+    expenses.pop(id)
+    save_expenses(expenses)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
